2ade32055a2852a2e5a33c48f8720b5171378ddf
webgurusam <shyamghosh666@gmail.com>
2022-02-07 20:18:24 +0600

    Simple Interest Calculation by JavaScript

--- simple-interest-calculation.js
+++ simple-interest-calculation.js
@@ -1,13 +1,20 @@
+// interest formula s.i = prt/100;
+// s.i = simple interest;
+// p = principle amount;
+// r = rate of interest out of 100%;
+// t = time period;
 
+// Simple Interest Calculation
+function getInterest(p, r, t) {
+    const myInterest = (p * r * t) / 100;
+    return myInterest;
+}
 
-
-
-
-
-
-
-
-
+const givenPrinciple = 15000; // your real amount which you will invest
+const givenRate = 10; // rate of interest out of 100%
+const givenTime = 2; // years
+const simpleInterest = getInterest(givenPrinciple, givenRate, givenTime);
+console.log('My interest amount is', simpleInterest, 'taka!!!');
 
 
 
